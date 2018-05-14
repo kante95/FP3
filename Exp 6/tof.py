aeaa6a308c7685e0ef5288a7c401fe4a1dade9db
marco <canterimarco@gmail.com>
2018-05-14 17:15:36 +0200

    analysis

--- Exp 6/tof.py
+++ Exp 6/tof.py
@@ -38,38 +38,65 @@ for i in range(len(file)):
 	v = -v
 	v = v[t>start_time]
 	t = t[t>start_time]
-	velocity = 0.3/(t - start_time)
+	velocity_below = 0.27/(t - start_time)
+	velocity_above = 0.33/(t - start_time)
+	v1 = v
 
-	v = v[velocity<800]
-	velocity =velocity[velocity< 800]
+	v = v[velocity_below<800]
+	velocity_below =velocity_below[velocity_below< 800]
 	if(file[i] == 31):
-		v = v[velocity>280]
-		velocity =velocity[velocity>280]
-	else:
-		v = v[velocity>350]
-		velocity =velocity[velocity>350]
-
-	plt.plot(velocity,v, label = str(width[i])+r" $\mu$s" ) 
+		v = v[velocity_below>180]
+		velocity_below =velocity_below[velocity_below>180]
+		v = v[velocity_below<500]
+		velocity_below =velocity_below[velocity_below<500]
 		
-	popt, pcov = curve_fit(floatmaxwell, velocity, v,method = "trf",p0=(0.01,2,450,0))
+	else:
+		v = v[velocity_below>250]
+		velocity_below =velocity_below[velocity_below>250]
+
+	v1 = v1[velocity_above<810]
+	velocity_above =velocity_above[velocity_above< 810]
+	if(file[i] == 31):
+		v1 = v1[velocity_above>280]
+		velocity_above =velocity_above[velocity_above>280]
+	else:
+		v1 = v1[velocity_above>350]
+		velocity_above =velocity_above[velocity_above>350]
+
+	plt.plot(velocity_below,v, label = str(width[i])+r" $\mu$s" ) 
+	plt.plot(velocity_above,v1, label = str(width[i])+r" $\mu$s" ) 
+		
+	popt, pcov = curve_fit(floatmaxwell, velocity_below, v,method = "trf",p0=(0.01,2,350,0))
+	#plt.plot(velocity,floatmaxwell(velocity_below,*popt),color="r")
+	popt1, pcov1 = curve_fit(floatmaxwell, velocity_above, v1,method = "trf",p0=(0.01,2,450,0))
+	#plt.plot(velocity,floatmaxwell(velocity_above,*popt1),color="r")
 	perr = np.sqrt(np.diag(pcov))
 
-	S = np.abs(popt[1])
-	u = popt[2]
-	plt.plot(velocity,floatmaxwell(velocity,*popt),color="r")
+	S = (np.abs(popt[1]) + np.abs(popt1[1]))/2
+	dS =np.abs( (np.abs(popt[1]) - np.abs(popt1[1]))/2) 
+	u = (popt[2] + popt1[2])/2
+	du = np.abs((popt[2] - popt1[2])/2)
+	
+
+	plt.plot(velocity_below,floatmaxwell(velocity_below,*popt),color="r")
+	plt.plot(velocity_above,floatmaxwell(velocity_above,*popt1),color="r")
 
 	plt.xlabel("Velocity [m/s]")
 	plt.ylabel("Voltage [V]")
 
 	#print("Pressure" + str(pressure[i]))
-	plt.legend()
+	#plt.legend()
 
 	soundspeed = np.sqrt((gamma*Temp)/(mk) )
 	mach = u/soundspeed
+	dmach = du/soundspeed
 	T = (0.5*mk)*(u/S)**2
+	dtdu = mk*(u/S**2)
+	dtds = mk*(u**2/S**3)
+	dT = np.sqrt( (dtdu*du)**2 + (dtds*dS)**2 )
 
 	#print("Pressure | width | S | u | T | Mach")
-	print(pressure[i],"&",width[i],"&", S,"&", u,"&",T,"&",mach,"\\\\")
+	print(pressure[i],"&",width[i],"&", S,"\\pm",dS,"&",u,"\\pm",du,"&",T,"\\pm",dT,"&",mach,"\\pm",dmach,"\\\\")
 
 #helium pressure 2.5
 
@@ -84,27 +111,47 @@ for k in v:
 v = -v
 v = v[t>start_time]
 t = t[t>start_time]
+v1 = v
 
-velocity = 0.3/(t - start_time)
-v = v[velocity<2000]
-velocity =velocity[velocity< 2000]
 
-plt.plot(velocity,v,label = r"230 $\mu$s") 
-popt, pcov = curve_fit(floatmaxwell, velocity, v,method = "trf",p0=(0.01,2,600,0))
+velocity_below = 0.27/(t - start_time)
+velocity_above = 0.33/(t - start_time)
+
+v = v[velocity_below<1100]
+velocity_below =velocity_below[velocity_below< 1100]
+v1 = v1[velocity_above<1100]
+velocity_above =velocity_above[velocity_above< 1100]
+
+
+plt.plot(velocity_above,v1,label = r"230 $\mu$s") 
+plt.plot(velocity_below,v,label = r"230 $\mu$s") 
+
+
+popt, pcov = curve_fit(floatmaxwell, velocity_below, v,method = "trf",p0=(0.01,2,600,0))
+popt1, pcov1 = curve_fit(floatmaxwell, velocity_above, v1,method = "trf",p0=(0.01,2,600,0))
 perr = np.sqrt(np.diag(pcov))
 
-S = np.abs(popt[1])
-u = popt[2]
-plt.plot(velocity,floatmaxwell(velocity,*popt),color="r")
+S = (np.abs(popt[1]) + np.abs(popt1[1]))/2
+dS =np.abs( (np.abs(popt[1]) - np.abs(popt1[1]))/2) 
+u = (popt[2] + popt1[2])/2
+du = np.abs((popt[2] - popt1[2])/2)
+
+
+plt.plot(velocity_above,floatmaxwell(velocity_above,*popt1),color="r")
+plt.plot(velocity_below,floatmaxwell(velocity_below,*popt),color="r")
 plt.xlabel("Velocity [m/s]")
 plt.ylabel("Voltage [V]")
 
 
 soundspeed = np.sqrt((gamma*Temp)/(mk) )
 mach = u/soundspeed
+dmach = du/soundspeed
 T = (0.5*mk)*(u/S)**2
+dtdu = mk*(u/S**2)
+dtds = mk*(u**2/S**3)
+dT = np.sqrt( (dtdu*du)**2 + (dtds*dS)**2 )
 
-print(230, S, u,T,mach)
+print(230,"&", S,"\\pm",dS,"&",u,"\\pm",du,"&",T,"\\pm",dT,"&",mach,"\\pm",dmach,"\\\\")
 
 ###################################################à
 t,v = read_data("data/ALL0037/F0037CH1.CSV")
@@ -116,22 +163,41 @@ for k in v:
 v = -v
 v = v[t>start_time]
 t = t[t>start_time]
-velocity = 0.3/(t - start_time)
-v = v[velocity<2000]
-velocity =velocity[velocity< 2000]
-plt.plot(velocity,v,label = r"220 $\mu$s") 
-popt, pcov = curve_fit(floatmaxwell, velocity, v,method = "trf",p0=(0.01,2,600,0))
+v1 = v
+
+
+velocity_below = 0.27/(t - start_time)
+velocity_above = 0.33/(t - start_time)
+
+v = v[velocity_below<1100]
+velocity_below =velocity_below[velocity_below< 1100]
+v1 = v1[velocity_above<1100]
+velocity_above =velocity_above[velocity_above< 1100]
+
+plt.plot(velocity_below,v,label = r"220 $\mu$s") 
+plt.plot(velocity_above,v1,label = r"220 $\mu$s") 
+popt, pcov = curve_fit(floatmaxwell, velocity_below, v,method = "trf",p0=(0.01,2,600,0))
+popt1, pcov1 = curve_fit(floatmaxwell, velocity_above, v1,method = "trf",p0=(0.01,2,600,0))
 perr = np.sqrt(np.diag(pcov))
 
-S = np.abs(popt[1])
-u = popt[2]
-plt.plot(velocity,floatmaxwell(velocity,*popt),color="r")
+S = (np.abs(popt[1]) + np.abs(popt1[1]))/2
+dS =np.abs( (np.abs(popt[1]) - np.abs(popt1[1]))/2) 
+u = (popt[2] + popt1[2])/2
+du = np.abs((popt[2] - popt1[2])/2)
+
+plt.plot(velocity_below,floatmaxwell(velocity_below,*popt),color="r")
+plt.plot(velocity_above,floatmaxwell(velocity_above,*popt1),color="r")
 
 soundspeed = np.sqrt((gamma*Temp)/(mk) )
 mach = u/soundspeed
+dmach = du/soundspeed
 T = (0.5*mk)*(u/S)**2
+dtdu = mk*(u/S**2)
+dtds = mk*(u**2/S**3)
+dT = np.sqrt( (dtdu*du)**2 + (dtds*dS)**2 )
 
-print(220, S, u,T,mach)
+print(220,"&", S,"\\pm",dS,"&",u,"\\pm",du,"&",T,"\\pm",dT,"&",mach,"\\pm",dmach,"\\\\")
+
 plt.legend()
 
 plt.show()
