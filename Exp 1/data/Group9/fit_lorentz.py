76567cd73c6f6b357f7a3a4071d5aca6cfbf31b3
lox2311 <sudotomato@keemail.me>
2018-05-18 16:46:14 +0200

    Some more analysis

--- Exp 1/data/Group9/fit_lorentz.py
+++ Exp 1/data/Group9/fit_lorentz.py
@@ -10,16 +10,18 @@ AngioI_HRes = 'AngioI_HRes/AngioI_HRes_000001.d/xy1.dx'
 AngioI_cal_HR_250 = "AngioI_cal_HR_250/AngioI_cal_HR_250x_000001.d/xy1.dx"
 
 
-mass, intensity = np.loadtxt(AngioI_cal_HR_250, delimiter = ',', unpack = True)
+mass1, intensity1 = np.loadtxt(AngioI_cal_HR_250, delimiter = ',', unpack = True)
 
-mass = mass[(mass > 432.92) & (mass < 432.93)]
-intensity = intensity[(mass > 432.92) & (mass < 432.93)]
+mass = mass1[(mass1 > 432.92) & (mass1 < 432.93)]
+intensity = intensity1[(mass1 > 432.92) & (mass1 < 432.93)]
 
-#params, pcov = sp.curve_fit(lorentz, mass, intensity, p0 = ())
+params, pcov = sp.curve_fit(lorentz, mass, intensity, p0 = (2e8, 0.0001, 432.925, 0))
+print("Resolution: " + str(params[2]/(2 * params[1])))
 
-plt.plot(mass, intensity, color = 'k')
+plt.plot(mass, intensity)
+plt.plot(mass, lorentz(mass, *params))
 plt.xlabel(r'$m/z$ in $\frac{\mathrm{u}}{e}$')
 plt.ylabel(r'Intensity')
 plt.tight_layout()
-#plt.savefig('hires_spectrum.pdf', format = 'pdf')
+#plt.savefig('hires_resolution.pdf', format = 'pdf')
 plt.show()
